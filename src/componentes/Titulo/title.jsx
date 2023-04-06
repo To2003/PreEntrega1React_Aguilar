cf3d3c6a956be3129b0c13e24c9553f26911fe3f
To2003 <soytomasaguilar@gmail.com>
2023-04-05 22:58:02 -0300

    first commit

---
+++ src/componentes/Titulo/title.jsx
@@ -0,0 +1,11 @@
+export const Titulo = ( )   => {
+
+    return (
+        <div class="logo">
+            <a href="./index.html">
+            
+                <h1>Nutrilicious</h1>
+            </a>            
+        </div>
+    )
+}
